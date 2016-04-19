560f0aec16c9d14368970be36af563b003711ba8
Jacob de Graaf <contact@jacobdegraaf.nl>
2016-04-19 14:14:16 +0200

    CRUD: Create and Read added ~ /wall/create and /wall/show, see routing.yml

--- Controller/SubscriptionWallController.php
+++ Controller/SubscriptionWallController.php
@@ -8,8 +8,33 @@
 
 namespace Integrated\Bundle\SubscriptionBundle\Controller;
 
+use Integrated\Bundle\SubscriptionBundle\Entity\SubscriptionWall;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Form\FormBuilderInterface;
+
 
 class SubscriptionWallController
 {
+    public function addAction(Request $request) {
 
+        $wall = new SubscriptionWall();
+
+        $form = $this->createFormBuilder($wall)
+            ->add('name', 'text')
+            ->add('save', 'submit')
+            ->getForm();
+
+        $form->handleRequest($request);
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($wall);
+            $em->flush();
+            return new Response('Wall added successfuly');
+        }
+
+        $build['form'] = $form->createView();
+        return $this->render('SubscriptionBundle:SubscriptionWall:wall_add.html.twig', $build);
+    }
 }
