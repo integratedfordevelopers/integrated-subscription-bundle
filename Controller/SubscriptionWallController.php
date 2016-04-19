5e20edb5829ba79bade5d714bbfe093f4ec620f8
Albert D. Bakker <albertdavidbakker@hotmail.com>
2016-04-19 12:42:50 +0200

    SubscriptionController added

--- Controller/SubscriptionWallController.php
+++ Controller/SubscriptionWallController.php
@@ -3,13 +3,13 @@
  * Created by PhpStorm.
  * User: Albert-David
  * Date: 19-04-16
- * Time: 12:37
+ * Time: 12:42
  */
 
 namespace Integrated\Bundle\SubscriptionBundle\Controller;
 
 
-class SubscriptionTypeController
+class SubscriptionWallController
 {
 
 }
