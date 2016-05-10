4a775607b05a5f552e7f101c3f19e207a7ab872d
Jacob de Graaf <contact@jacobdegraaf.nl>
2016-05-10 09:02:08 +0200

    Recoded according to the PSR-2 standard and removed some basic classes which where automatically generated.

--- Controller/SubscriptionWallController.php
+++ Controller/SubscriptionWallController.php
@@ -9,16 +9,15 @@
 namespace Integrated\Bundle\SubscriptionBundle\Controller;
 
 use Integrated\Bundle\SubscriptionBundle\Entity\SubscriptionWall;
-
 use Integrated\Bundle\SubscriptionBundle\Entity\WallChannel;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Form\Forms;
 
 class SubscriptionWallController extends Controller
 {
-    public function indexAction(){
+
+    public function indexAction()
+    {
         $walls = $this->getDoctrine()
             ->getRepository('IntegratedSubscriptionBundle:SubscriptionWall')
             ->findAll();
@@ -29,38 +28,36 @@ class SubscriptionWallController extends Controller
             throw $this->createNotFoundException('No walls found!');
         }
 
-        return $this->render('IntegratedSubscriptionBundle:SubscriptionWall:index.html.twig', array('walls' => $walls));
+        return $this->render('IntegratedSubscriptionBundle:SubscriptionWall:index.html.twig',
+            array('walls' => $walls));
     }
-    public function createAction(Request $request) {
 
+    public function createAction(Request $request)
+    {
         $wall = new SubscriptionWall();
-
         $form = $this->createFormBuilder($wall)
             ->add('name', 'text')
             ->add('teaser', 'textarea', array('label' => 'Description'))
             ->add('disabled', 'checkbox', array('required' => false))
-            ->add('freetier', 'text', array('required' => false))
-//            ->add('channels', 'integrated_channel_choice', ['multiple'=> true, 'expanded'=> true])
+            ->add('freetier', 'integer', array('required' => false))
+            ->add('channel', 'integrated_channel_choice', ['multiple'=> true, 'expanded'=> true, 'mapped' => false])
             ->add('save', 'submit')
             ->getForm();
-
-
         $form->handleRequest($request);
+
         if ($form->isValid()) {
-//            $channels = $wall->getChannels();
-//            $wall->setChannels(null);
             $em = $this->getDoctrine()->getManager();
             $em->persist($wall);
             $em->flush();
-//            foreach($channels as $channel) {
-//                $wallchannel = new WallChannel();
-//                $wallchannel->setChannel($channel->getId());
-//                $wallchannel->setWall($wall->getId());
-//                $wallchannel->setSubscriptionWall($wall);
-//                $em2 = $this->getDoctrine()->getManager();
-//                $em2->persist($wallchannel);
-//                $em2->flush();
-//            }
+            foreach($request->request->get("form")["channel"] as $channel) {
+                $wallchannel = new WallChannel();
+                $wallchannel->setChannel($channel);
+                $wallchannel->setWall($wall->getId());
+                $wallchannel->setSubscriptionWall($wall);
+                $em2 = $this->getDoctrine()->getManager();
+                $em2->persist($wallchannel);
+                $em2->flush();
+            }
 
             $this->addFlash(
                 'notice',
@@ -68,10 +65,10 @@ class SubscriptionWallController extends Controller
             );
             return $this->redirectToRoute('integrated_subscription_show_wall');
         }
-
         $build['form'] = $form->createView();
         return $this->render('IntegratedSubscriptionBundle:SubscriptionWall:create.html.twig', $build);
     }
+
     public function editAction()
     {
         $walls = $this->getDoctrine()
@@ -81,8 +78,10 @@ class SubscriptionWallController extends Controller
             throw $this->createNotFoundException('No walls found!');
         }
 
-        return $this->render('IntegratedSubscriptionBundle:SubscriptionWall:index.html.twig', array('walls' => $walls));
+        return $this->render('IntegratedSubscriptionBundle:SubscriptionWall:index.html.twig',
+            array('walls' => $walls));
     }
+
     public function deleteAction()
     {
         $walls = $this->getDoctrine()
@@ -92,6 +91,7 @@ class SubscriptionWallController extends Controller
             throw $this->createNotFoundException('No walls found!');
         }
 
-        return $this->render('IntegratedSubscriptionBundle:SubscriptionWall:index.html.twig', array('walls' => $walls));
+        return $this->render('IntegratedSubscriptionBundle:SubscriptionWall:index.html.twig',
+            array('walls' => $walls));
     }
 }
--- Tests/Controller/SubscriptionWallControllerTest.php
+++ Tests/Controller/SubscriptionWallControllerTest.php
@@ -2,19 +2,10 @@
 
 namespace Integrated\Bundle\SubscriptionBundle\Tests\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
-
-class SubscriptionWallControllerTest {
+class SubscriptionWallControllerTest extends \PHPUnit_Framework_TestCase {
     public function testIndex()
     {
-        static::createClient();
-        $client = static::createClient();
-
-        $crawler = $client->request('GET', '/hello/Fabien');
-
-        $this->assertTrue($crawler->filter('html:contains("Hello Fabien")')->count() > 0);
-        $this->assertTrue($crawler->filter('html:contains("Hello Fabien")')->count() > 0);
-        $this->assertEquals(10,10);
+        //$this->assertEquals(10,10);
 
     }
 }
