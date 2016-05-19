6f7b2340f0635f9b67fe7108f9cf2a1f23e864a2
Jacob de Graaf <contact@jacobdegraaf.nl>
2016-05-19 14:58:02 +0200

    Changes

--- Controller/SubscriptionWallController.php
+++ Controller/SubscriptionWallController.php
@@ -11,10 +11,23 @@
 
 namespace Integrated\Bundle\SubscriptionBundle\Controller;
 
+use Braincrafted\Bundle\BootstrapBundle\Session\FlashMessage;
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\Query;
 
+use Integrated\Bundle\ContentBundle\Doctrine\ChannelManager;
+use Integrated\Bundle\SubscriptionBundle\Model\SubscriptionWall;
+use Integrated\Bundle\SubscriptionBundle\Model\WallChannel;
+
+use Symfony\Bundle\FrameworkBundle\Routing\Router;
 use Symfony\Bundle\TwigBundle\TwigEngine;
+use Symfony\Component\Form\Form;
+use Symfony\Component\Form\FormFactory;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\Routing\RouterInterface;
+
 
 /**
  * @author Jacob de Graaf <jacob.de.graaf@windesheim.nl>
@@ -33,12 +46,40 @@ class SubscriptionWallController
     protected $em;
 
     /**
+     * @var ChannelManager
+     */
+    protected $cm;
+    /**
+     * @var Request
+     */
+    protected $request;
+
+    /**
+     * @var FormFactory
+     */
+    protected $form;
+    /**
+     * @var RouterInterface
+     */
+    protected $router;
+
+    /**
+     * @var FlashMessage
+     */
+    protected $flashMessage;
+
+    /**
      * @param TwigEngine $templating
      */
-    public function __construct(TwigEngine $templating, EntityManager $em)
+    public function __construct(TwigEngine $templating, EntityManager $em, ChannelManager $cm, FormFactory $form, RouterInterface $router, RequestStack $requestStack, FlashMessage $flashMessage)
     {
         $this->templating = $templating;
         $this->em = $em;
+        $this->cm = $cm;
+        $this->request = $requestStack->getCurrentRequest();
+        $this->form = $form;
+        $this->router = $router;
+        $this->flashMessage = $flashMessage;
     }
 
     /**
@@ -61,56 +102,55 @@ class SubscriptionWallController
      * @param Request $request
      * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
      */
-    public function createAction(Request $request)
+    public function createAction()
     {
         $wall = new SubscriptionWall();
-        $channels = $channels = $this->get("integrated_content.channel.manager")->findAll();
+        $channels = $this->cm->findAll();
         $channelNames = [];
+        
         foreach ($channels as $channel) {
             $channelNames[] = $channel->getName();
         }
 
         $form = $this->createCreateForm($wall, $channelNames);
-        $form->handleRequest($request);
+        $form->handleRequest($this->request);
 
         if ($form->isValid()) {
-            $em = $this->getDoctrine()->getManager();
-            $em->persist($wall);
-            $em->flush();
+            $this->em->persist($wall);
+            $this->em->flush();
 
-            if (isset($request->request->get("integrated_subscription_wall")["channel"])) {
-                foreach ($request->request->get("integrated_subscription_wall")["channel"] as $channel) {
+            if (isset($this->request->request->get("integrated_subscription_wall")["channel"])) {
+                foreach ($this->request->request->get("integrated_subscription_wall")["channel"] as $channel) {
                     $wallChannel = new WallChannel();
                     $wallChannel->setChannel($channelNames[$channel]);
                     $wallChannel->setWall($wall);
                     $wallChannel->setSubscriptionWall($wall);
-                    $em->persist($wallChannel);
-                    $em->flush();
+                    $this->em->persist($wallChannel);
+                    $this->em->flush();
                 }
             }
 
-            $this->get('braincrafted_bootstrap.flash')->success('Wall created');
+            $this->flashMessage->success('Wall created');
 
-            return $this->redirectToRoute('integrated_subscription_show_wall');
+            return new RedirectResponse($this->router->generate("integrated_subscription_show_wall"));
         }
         $build['form'] = $form->createView();
-        return $this->render('IntegratedSubscriptionBundle:SubscriptionWall:create.html.twig', $build);
+        return $this->templating->renderResponse('IntegratedSubscriptionBundle:SubscriptionWall:create.html.twig', $build);
     }
 
     /**
      * @param SubscriptionWall $wall
      * @param array $channelNames
      *
-     * @return \Symfony\Component\Form\Form
+     * @return Form
      */
     protected function createCreateForm(SubscriptionWall $wall, $channelNames)
     {
-
-        $form = $this->createForm(
+        $form = $this->form->create(
             'integrated_subscription_wall',
             $wall,
             [
-                'action' => $this->generateUrl('integrated_subscription_create_wall'),
+                'action' => $this->router->generate('integrated_subscription_create_wall'),
                 'method' => 'POST',
                 'attr' => $channelNames
             ]
